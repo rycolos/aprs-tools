b60d96a3c389435f86fac1e91555b73d992d8f84
Ryan LaLiberty <rlaliberty@gmail.com>
2022-09-18 11:40:23 -0400

    added yaml conf to msg

--- aprs_sender.py
+++ aprs_sender.py
@@ -1,10 +1,6 @@
-import socket, sys, textwrap
+import os, socket, sys, textwrap, yaml
 
-#CONFIG STATION AND SERVER - edit these
-callsign = "KC1QBY-1" #your station callsign
-passcode = "19369" #https://apps.magicbug.co.uk/passcode/
-serverHost = "noam.aprs2.net" #choose local server
-serverPort = 14580 #defined in aprs-is spec
+config_path = 'config.yaml'
 
 def get_inputs():
     """
@@ -18,12 +14,26 @@ def get_inputs():
         message = sys.argv[2]
     return dest, message
 
-def config_parse(callsign, passcode, message):
+def config_parse(config_path):
     """
-    parse to multiline packet
+    parse yaml config file for callsign, passcode, server details
+    """
+    if not os.path.isfile(config_path): 
+        sys.exit("Error: Config file " + config_path + "is missing. Please create it.")
+    with open(config_path, 'r') as file:
+        config_file = yaml.safe_load(file)
+    callsign = config_file['your_station_info']['callsign']
+    aprs_passcode = config_file['your_station_info']['aprs_passcode']
+    server_host = config_file['aprs_server']['server_host']
+    server_port = config_file['aprs_server']['server_port']
+    return callsign, aprs_passcode, server_host, server_port
+
+def packet_cons(callsign, aprs_passcode, dest, message):
+    """
+    construct multiline packet
     """
     address = callsign + ">APRS,TCPIP::"
-    login = "user " + callsign + " pass " + passcode + " vers aprs_send 1.0"
+    login = "user " + callsign + " pass " + str(aprs_passcode) + " vers aprs_send 1.0"
     final_dest = dest.ljust(9)
     packet = address + final_dest + ":" + message 
     ml_packet = textwrap.dedent(f"""
@@ -32,17 +42,15 @@ def config_parse(callsign, passcode, message):
     """)
     return ml_packet
 
-def send_message(ml_packet, serverHost, serverPort):
+def send_message(ml_packet, server_host, server_port):
     """
     open socket to local aprs.is server, send packet, hold open for 10s
     """
-    print("Packet to be sent:", ml_packet)
-
+    print("\nPacket to be sent:", ml_packet)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(10)
-    s.connect((serverHost, serverPort))
+    s.connect((server_host, server_port))
     s.sendall(ml_packet.encode())
-
     while 1:
         try:
             data = s.recv(4096)
@@ -54,12 +62,17 @@ def send_message(ml_packet, serverHost, serverPort):
 
 def main():
     dest,message = get_inputs()
-    ml_packet = (config_parse(callsign, passcode, message))
-    send_message(ml_packet, serverHost, serverPort)
+    callsign,aprs_passcode,server_host,server_port = config_parse(config_path)
+    ml_packet = (packet_cons(callsign, aprs_passcode, dest, message))
+    send_message(ml_packet, server_host, server_port)
 
 
 if __name__ == "__main__":
     main()
 
-
-#credit: https://www.aprs-is.net/Connecting.aspx, simpleaprs
+"""
+credit: 
+https://www.aprs-is.net/Connecting.aspx for technical spec
+https://github.com/wa1gov/Simple-Shell-APRS/ for bash reference implementation
+https://aprs.fi for cloud APIs and testing tools
+"""
